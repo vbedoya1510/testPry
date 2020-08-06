c1b115aea56fd1dd87e193fe4b0f5711710ecd8a
vbedoya <vbedoya@transfiriendo.com>
2020-08-06 10:24:48 -0500

    ! Se actualizan los archivos del proyecto

--- FrontPry/pryCallCenter/src/app/app.module.ts
+++ FrontPry/pryCallCenter/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file/file-upload/file-upload.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
